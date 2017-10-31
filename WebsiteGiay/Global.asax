f2d343d9ca94de4fa844a4d4889cca80249d9226
ptcamtu97 <ptcamtu97@gmail.com>
2017-10-31 22:06:41 +0700

    1

---
+++ WebsiteGiay/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebsiteGiay.MvcApplication" Language="C#" %>
---
+++ WebsiteGiay/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebsiteGiay.Startup))]
+namespace WebsiteGiay
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
